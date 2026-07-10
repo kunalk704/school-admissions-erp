813afd99423253842cd30cf0ab341921e830627a
kunalk704 <krishnakunal0704@gmail.com>
2026-07-10 10:16:06 +0530

    migration of grade

---
+++ backend/database/migrations/2026_07_09_121122_create_grades_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('school_id')->constrained();
+            $table->string('name', 50);
+            $table->unique(['school_id', 'name']);
+            $table->integer('display_order');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
