d5424f1dc66c6273c2c6faef849b80c39345c5fe
kunalk704 <krishnakunal0704@gmail.com>
2026-07-10 11:20:51 +0530

    migration of section table

---
+++ backend/database/migrations/2026_07_10_054223_create_sections_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('grade_id')->constrained();
+            $table->string('name', 20);
+            $table->unique(['grade_id', 'name']);
+            $table->integer('display_order')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
